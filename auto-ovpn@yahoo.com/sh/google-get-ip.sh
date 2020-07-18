e8c097adafd4b5b212cca1476bb13de00f795c19
Jason Mun <30671857+yomun@users.noreply.github.com>
2020-07-19 01:51:20 +0800

    some ip started to offer IPV6

--- auto-ovpn@yahoo.com/sh/google-get-ip.sh
+++ auto-ovpn@yahoo.com/sh/google-get-ip.sh
@@ -47,21 +47,16 @@ else
 	RAN=`echo $((1 + RANDOM % 16))`
 	case ${RAN} in
 		1)  IP=`curl http://ipinfo.io/ip`;;
-		2)  IP=`curl http://ifconfig.co/`;;
-		3)  IP=`curl http://icanhazip.com/`;;
-		4)  IP=`curl https://showip.net/`;;
-		5)  IP=`curl https://api.ipify.org`;;
-		6)  IP=`curl http://checkip.dyndns.com/                | grep -Eo "(IP Address: )[0-9.]*"         | sed "s/IP Address: //g"`;;
-		7)  IP=`curl http://checkip.dyndns.org/                | grep -Eo "(IP Address: )[0-9.]*"         | sed "s/IP Address: //g"`;;
-		8)  IP=`curl http:/checkip.dyn.com/                    | grep -Eo "(IP Address: )[0-9.]*"         | sed "s/IP Address: //g"`;;
-		9)  IP=`curl http://ipecho.net/                        | grep -Eo "(Your IP is )[0-9.]*"          | sed "s/Your IP is //g"`;;
-		10) IP=`curl https://l2.io/ip.js?var=myip              | grep -Eo "[0-9.]*"`;;
-		11) IP=`curl http://www.showmemyip.com/                | grep -Eo "(Your IP address is: )[0-9.]*" | grep -Eo "[0-9.]*"`;;
-		12) IP=`curl https://api.userinfo.io/userinfos         | grep -Eo "(\"ip_address\":\")[0-9.]*"    | grep -Eo "[0-9.]*"`;;
-		13) IP=`curl http://www.showmyip.gr/                   | grep "Your IP is: "                      | grep -Eo "[0-9.]*"`;;
-		14) IP=`curl http://www.showip.com/                    | grep "Your IP address is "               | grep -Eo "[0-9.]*"`;;
-		15) IP=`curl http://www.geoplugin.com/webservices/json | grep "\"geoplugin_request\":\""          | grep -Eo "[0-9.]*"`;;
-		*)  IP=`curl http://www.vpngate.net/en/                | grep -Eo "(Your IP: )[0-9.]*"            | sed "s/Your IP: //g"`;;
+		2)  IP=`curl https://api.ipify.org`;;
+		3)  IP=`curl http://checkip.dyndns.com/                | grep -Eo "(IP Address: )[0-9.]*"         | sed "s/IP Address: //g"`;;
+		4)  IP=`curl http://checkip.dyndns.org/                | grep -Eo "(IP Address: )[0-9.]*"         | sed "s/IP Address: //g"`;;
+		5)  IP=`curl http:/checkip.dyn.com/                    | grep -Eo "(IP Address: )[0-9.]*"         | sed "s/IP Address: //g"`;;
+		6)  IP=`curl http://ipecho.net/                        | grep -Eo "(Your IP is )[0-9.]*"          | sed "s/Your IP is //g"`;;
+		7)  IP=`curl https://l2.io/ip.js?var=myip              | grep -Eo "[0-9.]*"`;;
+		8)  IP=`curl http://www.showmemyip.com/                | grep -Eo "(Your IP address is: )[0-9.]*" | grep -Eo "[0-9.]*"`;;
+		9)  IP=`curl https://api.userinfo.io/userinfos         | grep -Eo "(\"ip_address\":\")[0-9.]*"    | grep -Eo "[0-9.]*"`;;
+		10) IP=`curl http://www.showip.com/                    | grep "Your IP address is "               | grep -Eo "[0-9.]*"`;;
+		*)  IP=`curl http://www.geoplugin.com/webservices/json | grep "\"geoplugin_request\":\""          | grep -Eo "[0-9.]*"`;;
 	esac
 
 	if [[ ${IP} =~ ${IP_RE} ]]
